90d7662cf0efdcbf9b386702d35f8bf22b7f0182
sort7er <98805454+sort7er@users.noreply.github.com>
2022-04-27 16:58:46 +0200

    PressurePlatePrefab

--- Mandatory5/Assets/UpperRegion/Scripts/PressurePlate.cs
+++ Mandatory5/Assets/UpperRegion/Scripts/PressurePlate.cs
@@ -4,8 +4,10 @@ using UnityEngine.Events;
 public class PressurePlate : MonoBehaviour
 {
     public Animator plateAnim;
+    public float getOffDelay = 0;
     public UnityEvent onPressurePlate;
     public UnityEvent offPressurePlate;
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player") 
@@ -18,8 +20,12 @@ public class PressurePlate : MonoBehaviour
     {
         if (other.gameObject.tag == "Player")
         {
-            plateAnim.SetBool("IsActive", false);
-            offPressurePlate.Invoke();
+            Invoke("Off", getOffDelay);
         }
+    }
+    private void Off()
+    {
+        plateAnim.SetBool("IsActive", false);
+        offPressurePlate.Invoke();
     }
 }
