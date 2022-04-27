f2180bce1d6da9f398a727cb599bfc9c0cb8f143
sort7er <98805454+sort7er@users.noreply.github.com>
2022-04-27 16:41:43 +0200

    PressurePlate

---
+++ Mandatory5/Assets/UpperRegion/Scripts/PressurePlate.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PressurePlate : MonoBehaviour
+{
+    public Animator plateAnim;
+    public UnityEvent onPressurePlate;
+    public UnityEvent offPressurePlate;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player") 
+        {
+            plateAnim.SetBool("IsActive", true);
+            onPressurePlate.Invoke();
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            plateAnim.SetBool("IsActive", false);
+            offPressurePlate.Invoke();
+        }
+    }
+}
