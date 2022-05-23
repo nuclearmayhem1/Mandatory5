cac1b8249bed83ce2d5f9ce092bee401ef831d12
nuclearmayhem1 <53118215+nuclearmayhem1@users.noreply.github.com>
2022-05-23 12:30:32 +0200

    updating

--- Mandatory5/Assets/Overworld/Kitchen/Scripts/ObjectManipulation.cs
+++ Mandatory5/Assets/Overworld/Kitchen/Scripts/ObjectManipulation.cs
@@ -1,13 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using StarterAssets;
 
 public class ObjectManipulation : MonoBehaviour
 {
     [SerializeField] GameObject grabPoint, cameraRoot;
     [SerializeField] float grabStrenght = 5;
     [SerializeField] private float rayCastRange = 5;
+    private float grabPointDistance = 3f;
+    [SerializeField] private float grabPointMin, grabPointMax;
+    [SerializeField] private ThirdPersonController playerController;
 
+    private void OnValidate()
+    {
+        if (grabPointMin <= 0)
+        {
+            grabPointMin = 0;
+        }
+        if (grabPointMax <= 1)
+        {
+            grabPointMax = 1;
+        }
+        if (grabPointMin > grabPointMax - 1)
+        {
+            grabPointMax = grabPointMin + 1;
+        }
+        if (grabPointMax < grabPointMin + 1)
+        {
+            grabPointMin = grabPointMax - 1;
+        }
+    }
+
+    private bool rightClick = false;
     private bool holding = false;
     private void Update()
     {
@@ -20,18 +45,33 @@ public class ObjectManipulation : MonoBehaviour
             holding = DropObject();
         }
 
+        grabPointDistance += Input.GetAxis("Mouse ScrollWheel");
+        grabPointDistance = Mathf.Clamp(grabPointDistance, grabPointMin, grabPointMax);
+        grabPoint.transform.localPosition = new Vector3(0, 0, grabPointDistance);
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            playerController.freezePlayerCamera = true;
+            rightClick = true;
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            playerController.freezePlayerCamera = false;
+            rightClick = false;
+        }
+        
         if (holding)
         {
             Rigidbody heldRb = held.GetComponent<Rigidbody>();
             heldRb.velocity =  (grabPoint.transform.position - held.transform.position) * grabStrenght;
+            heldRb.freezeRotation = true;
 
+            if (rightClick)
+            {
 
-            //heldRb.isKinematic = true;
-            //heldRb.MovePosition(grabPoint.transform.position);
-            //Vector3.Lerp(held.transform.position, grabPoint.transform.position, 0.1f);
+            }
+
         }
-
-
     }
 
 
@@ -66,7 +106,7 @@ public class ObjectManipulation : MonoBehaviour
 
     private bool DropObject()
     {
-        //held.GetComponent<Rigidbody>().isKinematic = false;
+        held.GetComponent<Rigidbody>().freezeRotation = false;
         held = null;
         return false;
     }
@@ -75,5 +115,12 @@ public class ObjectManipulation : MonoBehaviour
     {
         Gizmos.color = Color.green;
         Gizmos.DrawLine(transform.position, cameraRoot.transform.rotation * Vector3.forward * rayCastRange + transform.position);
+        Gizmos.DrawWireSphere(grabPoint.transform.position, 0.1f);
+        Physics.Raycast(new Ray(transform.position, cameraRoot.transform.forward), out RaycastHit hit, rayCastRange);
+        if (hit.collider != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawCube(hit.point, Vector3.one * 0.1f);
+        }
     }
 }
