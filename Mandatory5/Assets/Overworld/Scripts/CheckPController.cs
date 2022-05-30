703843063a0c750231a41600decce886ee9d2308
StigAndreR <349597@student.nord.no>
2022-05-30 16:02:21 +0200

    ship progress works.
    The appropriate progression items needs to be put into the correct placements.

---
+++ Mandatory5/Assets/Overworld/Scripts/CheckPController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPController : MonoBehaviour
+{
+    Vector3 playerStartPoint;
+    
+
+    private void Start()
+    {
+        string checkpoint = PlayerPrefs.GetString("CheckPoint", null);
+        if (checkpoint != PlayerPrefs.GetString("CheckPoint", null) || PlayerPrefs.HasKey("CheckPoint") != true)
+        {
+            
+        }
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        string checkopintCheck = gameObject.name;
+        if (other.CompareTag("Player"))
+        {
+            playerStartPoint = gameObject.transform.position;
+            PlayerPrefs.SetString("CheckPoint", checkopintCheck);
+        }
+    }
+}
