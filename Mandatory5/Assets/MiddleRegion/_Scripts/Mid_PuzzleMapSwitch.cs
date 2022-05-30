3ad5226ca332825f9484d193cb81525241fe658e
Granderous <kevin.b.grande@student.nord.no>
2022-05-30 11:45:37 +0200

    PuzzleChanger
    Added a system for changing the puzzle areas.

---
+++ Mandatory5/Assets/MiddleRegion/_Scripts/Mid_PuzzleMapSwitch.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mid_PuzzleMapSwitch : MonoBehaviour
+{
+    //private GameObject puzzleSpawnLocation;
+    public GameObject firstPuzzleArea, secondPuzzleArea, thirdPuzzleArea, puzzleAreaDoor, closeTheDoorTrigger;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //puzzleSpawnLocation = GameObject.Find("PuzzleSpawnLocation");
+        firstPuzzleArea.SetActive(false);
+        secondPuzzleArea.SetActive(false);
+        thirdPuzzleArea.SetActive(false);
+        puzzleAreaDoor.SetActive(true);
+        closeTheDoorTrigger.SetActive(false); 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            SpawnFirstPuzzle();
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            SpawnSecondPuzzle();
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            SpawnThirdPuzzle();
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            OpenPuzzleDoor();
+        }
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            ActivateDoorTrigger();
+        }
+
+
+
+    }
+
+
+    public void SpawnFirstPuzzle()
+    {
+        firstPuzzleArea.SetActive(true);
+        secondPuzzleArea.SetActive(false);
+        thirdPuzzleArea.SetActive(false);
+
+    }
+    public void SpawnSecondPuzzle() 
+    {
+        firstPuzzleArea.SetActive(false);
+        secondPuzzleArea.SetActive(true);
+        thirdPuzzleArea.SetActive(false);
+
+    }
+    public void SpawnThirdPuzzle() 
+    {
+        firstPuzzleArea.SetActive(false);
+        secondPuzzleArea.SetActive(false);
+        thirdPuzzleArea.SetActive(true);
+
+    }
+
+    public void OpenPuzzleDoor()
+    {
+        puzzleAreaDoor.SetActive(false);
+    }
+
+    public void ActivateDoorTrigger()
+    {
+        closeTheDoorTrigger.SetActive(true);
+    }
+    
+
+
+}
