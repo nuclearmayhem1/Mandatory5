d5b87aab5de92e6dbbdad317342622fa5761f4d5
tusjmod <64898919+tusjmod@users.noreply.github.com>
2022-06-07 04:38:31 +0200

    capstan work 2

--- Mandatory5/Assets/UpperRegion/Scripts/Tormod/Capstan.cs
+++ Mandatory5/Assets/UpperRegion/Scripts/Tormod/Capstan.cs
@@ -2,7 +2,12 @@ using UnityEngine;
 
 public class Capstan : MonoBehaviour {
 
+    [SerializeField] private Vector2 rotationRange;
+    [SerializeField] private Vector2 outputValueRange;
     [SerializeField] private CapstanHandle[] capstanHandles;
+
+    private float _yRotation; //Might have to locally track as unity rotation loops when going outside 0-360deg
+    
     
     
 }
--- Mandatory5/Assets/UpperRegion/Scripts/Tormod/CapstanHandle.cs
+++ Mandatory5/Assets/UpperRegion/Scripts/Tormod/CapstanHandle.cs
@@ -15,6 +15,7 @@ public class CapstanHandle : MonoBehaviour {
     private bool _inRange;
     private PlayerInput _playerInput;
     private Transform _pivot;
+    private int _direction;
 
     private void Awake() {
         _pivot = transform.parent;
@@ -49,9 +50,10 @@ public class CapstanHandle : MonoBehaviour {
 
     private void CheckInteraction() {
         var keyPressed = Input.GetKeyDown(interactionKey);
-        if (keyPressed && _inRange) {
-            if (_currentHandle == this) ExitPushState();
-            else EnterPushState();
+        if (keyPressed) {
+            if (_currentHandle != this && _inRange) 
+                EnterPushState();
+            else ExitPushState();
         }
     }
 
@@ -60,12 +62,34 @@ public class CapstanHandle : MonoBehaviour {
         OnIndicator?.Invoke(false, interactionKey);
         OnPushState?.Invoke(true);
         _playerInput.enabled = false;
-        _playerInput.transform.SetParent(_pivot); //transform.parent is pivot
+        var playerTransform = _playerInput.transform;
+        playerTransform.SetParent(_pivot); //transform.parent is pivot
+        
+        //Find which side of the handle + position the player
+        var handlePosition = transform.position;
+        var playerPosition = playerTransform.position;
+        var vectorToTarget = (handlePosition - playerPosition).normalized;
+        handlePosition.y = playerPosition.y;
+        playerTransform.position = handlePosition;
+
+        if (Vector3.Dot(vectorToTarget, transform.forward) > 0) { //Where is player related to the handle?
+            //Reverse direction
+            _direction = -1;
+            playerTransform.position -= transform.forward * .2f;
+        } else {
+            //Normal direction
+            _direction = 1;
+            playerTransform.position += transform.forward * .2f;
+        }
+
+        playerTransform.LookAt(handlePosition);
+        
+        //Did last: made rotate correct way related to which side. New bug: interacting while walking along handle launches player far
     }
 
     private void ExitPushState() {
         _currentHandle = null;
-        OnIndicator?.Invoke(true, interactionKey);
+        OnIndicator?.Invoke(_inRange, interactionKey); //Pass _inRange in case outside range
         OnPushState?.Invoke(false);
         _playerInput.enabled = true;
         _playerInput.transform.SetParent(null);
@@ -75,10 +99,10 @@ public class CapstanHandle : MonoBehaviour {
     private void HandlePushState() {
         if (Input.GetKey(KeyCode.W)) {
             //Push forward
-            _pivot.Rotate(-Vector3.up * Time.deltaTime * rotationSpeed);
+            _pivot.Rotate(-Vector3.up * Time.deltaTime * rotationSpeed * _direction);
         } else if (Input.GetKey(KeyCode.S)) {
             //Push backward
-            _pivot.Rotate(Vector3.up * Time.deltaTime * rotationSpeed);
+            _pivot.Rotate(Vector3.up * Time.deltaTime * rotationSpeed * _direction);
         }
     }
 }
