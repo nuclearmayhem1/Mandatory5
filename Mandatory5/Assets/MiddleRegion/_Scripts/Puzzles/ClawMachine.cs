c623559ecbd9af9a9380725ff4278c66a6124359
HenBak <henrik.b.bakke@student.nord.no>
2022-06-13 12:14:27 +0200

    claw machine

---
+++ Mandatory5/Assets/MiddleRegion/_Scripts/Puzzles/ClawMachine.cs
@@ -0,0 +1,17 @@
+
+using UnityEngine;
+
+public class ClawMachine : MonoBehaviour
+{
+   
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
---
+++ Mandatory5/Assets/MiddleRegion/_Scripts/Puzzles/ClawMachineClaw.cs
@@ -0,0 +1,17 @@
+
+using UnityEngine;
+
+public class ClawMachineClaw : MonoBehaviour
+{
+ 
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
