72f15806cfb033b08353f56572021dc0d66f7b2f
loldani123 <harestaddaniel@gmail.com>
2022-06-14 00:07:04 +0200

    P4 prefab
    puzzle 4 is now a prefab

--- Mandatory5/Assets/LowerRegion/Scripts/ToggleShroom.cs
+++ Mandatory5/Assets/LowerRegion/Scripts/ToggleShroom.cs
@@ -15,6 +15,8 @@ public class ToggleShroom : MonoBehaviour
             {
                 PlayerDrugChecker.isHigh = false;
             }
+
+            GetComponent<ParticleSystem>().Play();
         }
     }
 
