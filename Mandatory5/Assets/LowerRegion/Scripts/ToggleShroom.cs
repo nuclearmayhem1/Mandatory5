21ae0fa7a0d1f59824ddf4e27ab954349ec25e71
loldani123 <harestaddaniel@gmail.com>
2022-06-14 12:36:43 +0200

    puz4 prefab

--- Mandatory5/Assets/LowerRegion/Scripts/ToggleShroom.cs
+++ Mandatory5/Assets/LowerRegion/Scripts/ToggleShroom.cs
@@ -5,6 +5,20 @@ using UnityEngine;
 public class ToggleShroom : MonoBehaviour
 {
     public GameObject sporeZone;
+    public GameObject sporeAura;
+    private bool isBlue = true;
+
+    private void Update()
+    {
+        if (PlayerDrugChecker.isHigh == true)
+        {
+            SetAuraColor("yellow");
+        }
+        else
+        {
+            SetAuraColor("blue");
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -17,6 +31,20 @@ public class ToggleShroom : MonoBehaviour
             }
 
             GetComponent<ParticleSystem>().Play();
+        }
+    }
+
+    private void SetAuraColor( string color)
+    {
+        if (color == "blue")
+        {
+            sporeAura.GetComponent<Renderer>().material.color = new Color(0.15f, 0.18f, 0.70f);
+            isBlue = true;
+        }
+        else if (color == "yellow")
+        {
+            sporeAura.GetComponent<Renderer>().material.color = new Color(0.85f, 0.85f, 0.27f);
+            isBlue = false;
         }
     }
 
