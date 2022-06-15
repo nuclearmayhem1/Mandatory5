3d648bdc6b25c7141e9bb5cfeaec7e9866815a90
BastianEngoe <92574751+BastianEngoe@users.noreply.github.com>
2022-06-15 15:47:09 +0200

    Code comments and nav mesh

--- Mandatory5/Assets/LowerRegion/Scripts/ShroomQuest.cs
+++ Mandatory5/Assets/LowerRegion/Scripts/ShroomQuest.cs
@@ -12,17 +12,22 @@ public class ShroomQuest : MonoBehaviour
 
     private void Start()
     {
-        QuestMenuRenderer.currentWorld = Quest.World.LowerWorld;
+        QuestMenuRenderer.currentWorld = Quest.World.LowerWorld;            //Sets the current region in the questmanager.
     }
 
     void Update()
     {
-        if (dialogue.GetComponent<MidDialogue>().dialogueNumber == 4 && acceptedQuest == false)
-        {
+        if (dialogue.GetComponent<MidDialogue>().dialogueNumber == 4 && acceptedQuest == false)  //Checks for dialogue line then give quests and flip bool to not make it repeat
+        {                                          
             shroomQuest = QuestManager.AddQuest(new Quest(Quest.World.LowerWorld, "Get the golden mushroom on the top of the mountain"));
             Destroy(gate1);
-            Destroy(gate2);
+            Destroy(gate2);             //Destroys gates to let player in, forcing them to pick up this quest.
             acceptedQuest = true;
+        }
+
+        if (acceptedQuest == true && ShroomQuestCompletion.shroomQuestObjectiveComplete == true) //Gets quest objective completion from ShroomQuestCompletion.cs and sets quests as complete.
+        {
+            QuestManager.SetNormalQuestStatus(shroomQuest, true);
         }
     }
 }
