6aafec0446c38ded8480b52e3dcd4791f59bfcfc
loldani123 <harestaddaniel@gmail.com>
2022-06-15 15:52:44 +0200

    checkpoints and minor thigns

---
+++ Mandatory5/Assets/LowerRegion/Scripts/ToggleShroomQuest.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Quests;
+
+public class ToggleShroomQuest : MonoBehaviour
+{
+    //this script gives the player a quest to cross the bog after they talk with the funguy.
+    private bool acceptedQuest;
+    public GameObject dialogue;
+    private uint toggleShroomQuest = 4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dialogue.GetComponent<MidDialogue>().dialogueNumber == 4 && acceptedQuest == false) 
+        {
+            toggleShroomQuest = QuestManager.AddQuest(new Quest(Quest.World.LowerWorld, "Find a way to cross the bog"));
+            acceptedQuest = true;
+        }
+    }
+}
