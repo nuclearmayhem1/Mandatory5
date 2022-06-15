7670ad948f9cbb7be21c6f6c639a5a0d88eef953
hoangyenpham51 <347913@student.nord.no>
2022-06-16 01:09:05 +0700

    Added description to my scripts

--- Mandatory5/Assets/MiddleRegion/Yen/YEN_Mid_VolcanoBorder.cs
+++ Mandatory5/Assets/MiddleRegion/Yen/YEN_Mid_VolcanoBorder.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class YEN_Mid_VolcanoBorder : MonoBehaviour
+public class YEN_Mid_VolcanoBorder : MonoBehaviour //The skybox in the game will be changed when the player enter the Volcano area
 {
     public Material[] skyboxes;
     public Light volcanoLight;
